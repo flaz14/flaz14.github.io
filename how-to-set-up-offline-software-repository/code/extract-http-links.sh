16dd67235b4c936ba91997ebaa77aa485d1de571
flaz14 <flazfourteen@gmail.com>
2016-12-25 17:29:01 +0300

    Refine utility script

--- how-to-set-up-offline-software-repository/code/extract-http-links.sh
+++ how-to-set-up-offline-software-repository/code/extract-http-links.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-regex=".*(http://[a-z.]*) +"
+declare -r http_link_regex='.*(http://[a-z.]*) +'
 declare -a http_links
 
 while read line; do
-	[[ $line =~ $regex ]]
+	[[ $line =~ $http_link_regex ]] || continue
 	http_link="${BASH_REMATCH[1]}"
-	[[ "$http_link" = "" ]] && continue
-	# trim leading/trailing whitespaces and squeeze iternal whitespaces
+	# trim leading/trailing whitespaces and squeeze internal whitespaces
 	pretty_link="$(echo "$http_link" | xargs)" 
 	http_links+=("$pretty_link")
 done < apt-get_update_sample.log
 
 printf '%s\n' "${http_links[@]}" | sort | uniq
-
-
