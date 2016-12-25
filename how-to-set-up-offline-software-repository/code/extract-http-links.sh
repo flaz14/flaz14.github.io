5d554d830e5c1365e95dfc1d2545b03c81467767
flaz14 <flazfourteen@gmail.com>
2016-12-25 13:57:36 +0300

    Publish draft of utility script

---
+++ how-to-set-up-offline-software-repository/code/extract-http-links.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+regex=".*(http://[a-z.]*) +"
+declare -a http_links
+
+while read line; do
+	[[ $line =~ $regex ]]
+	http_link="${BASH_REMATCH[1]}"
+	[[ "$http_link" = "" ]] && continue
+	# trim leading/trailing whitespaces and squeeze iternal whitespaces
+	pretty_link="$(echo "$http_link" | xargs)" 
+	http_links+=("$pretty_link")
+done < apt-get_update_sample.log
+
+printf '%s\n' "${http_links[@]}" | uniq
+
+
