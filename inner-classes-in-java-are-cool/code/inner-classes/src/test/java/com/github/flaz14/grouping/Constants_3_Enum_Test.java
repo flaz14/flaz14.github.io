1e0fc916f332dab447bcffe35ce395b76d178cda
flaz14 <flazfourteen@gmail.com>
2017-03-04 18:46:48 +0300

    inner-classes-in-java-are-cool: one more chapter, ignore build files

---
+++ inner-classes-in-java-are-cool/code/inner-classes/src/test/java/com/github/flaz14/grouping/Constants_3_Enum_Test.java
@@ -0,0 +1,25 @@
+package com.github.flaz14.grouping;
+
+import org.junit.Test;
+
+public class Constants_3_Enum_Test {
+
+    private enum CustomerId {
+        VALID("12345"),
+        INVALID("!!!"),
+        NON_EXISTING("000");
+
+        public final String id;
+
+        private CustomerId(String id) {
+            this.id = id;
+        }
+    }
+
+    @Test
+    public void someTest() throws Exception {
+        System.out.println(CustomerId.VALID.id);
+        System.out.println(CustomerId.INVALID.id);
+        System.out.println(CustomerId.NON_EXISTING.id);
+    }
+}
