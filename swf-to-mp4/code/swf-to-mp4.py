4244f67abaa3ff0d0eb49fdc565d9e8aee496f75
flaz14 <flazfourteen@gmail.com>
2017-04-08 19:40:38 +0300

    swf-to-mp4: mostly finish exploring SWF file

--- swf-to-mp4/code/swf-to-mp4.py
+++ swf-to-mp4/code/swf-to-mp4.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python3
 
-#from subprocess import call as sh
-
-
 import subprocess
 import re
 
-output = subprocess.check_output(["ls", "-l"])
-print("output is [{0}]".format(output))
-
-
 # explore SWF-file
-width_raw = subprocess.check_output(['swfdump', '--width', '../video/tricky.swf'])
-print(width_raw)
-width_pretty = str(width_raw, "utf-8") # TODO write a remark about used encoding
+raw_info = subprocess.check_output(['swfdump', '--width', '--height', '--rate', '../video/tricky.swf'])
+print(raw_info)
+pretty_info = str(raw_info, "utf-8") # TODO write a remark about used encoding
+parsed_info = re.compile("-X (?P<width>\d+) -Y (?P<height>\d+) -r (?P<rate>\d+\.\d+)", re.S).search(pretty_info).groupdict()
+print(parsed_info)
 
-
-
-width_parsed = re.compile("-X (\d+)", re.S).search(width_pretty).groups()[0]
-print(width_parsed)
-
-width = int(width_parsed)
-print(width)
