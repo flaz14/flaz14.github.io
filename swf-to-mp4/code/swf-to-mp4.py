ce469e6b0bf05056ebc11f0acabfad476a7a8649
flaz14 <flazfourteen@gmail.com>
2017-04-16 21:12:27 +0300

    swf-to-mp4: first version of the script that works mostly

--- swf-to-mp4/code/swf-to-mp4.py
+++ swf-to-mp4/code/swf-to-mp4.py
@@ -17,13 +17,14 @@ def explore_swf_file(filename):
 	return swf
 	
 class Screen:
-	def __init__(self, width, height, filename):
-		self.width = width
-		self.height = height
+	def __init__(self, swf, filename):
+		self.swf = swf
 		xvfb_command = [
 			'Xvfb',
 			':44',
-			'-screen', '0', '{}x{}x24'.format(width, height)
+			'-screen', '0', '{}x{}x24'.format(self.swf['width'], self.swf['height']),
+			'-pixdepths', '3', '27',
+			'-fbdir', '/tmp'
 		]
 		self.xvfb = subprocess.Popen(xvfb_command, stdout=sys.stdout)
 		time.sleep(10)
@@ -32,42 +33,44 @@ class Screen:
 			'--once',
 			'--x-pos', '0',
 			'--y-pos', '0',
-			'--fullscreen',
+			'--width', str(self.swf['width']),
+			'--height', str(self.swf['height']),
+			#'--fullscreen',
+			'--hide-menubar',
 			'--verbose',
 			filename
 		]
-		#virtual_display = {'DISPLAY': ':44'}
-		#self.gnash = subprocess.Popen(gnash_command, env = virtual_display, stdout=sys.stdout)
-		self.gnash = subprocess.Popen(gnash_command, stdout=sys.stdout)
+		virtual_display = {'DISPLAY': ':44'}
+		self.gnash = subprocess.Popen(gnash_command, env = virtual_display, stdout=sys.stdout)
 		
 	def start_playing(self):
 		time.sleep(10)
-		#virtual_display = {'DISPLAY': ':44'}
+		virtual_display = {'DISPLAY': ':44'}
 		xdotool_command = [
 			'xdotool',
-			'mousemove', str(self.width / 2), str(self.height / 2),
+			'mousemove', '0', '0',
+			'mousemove', str(self.swf['width'] - 20), str(self.swf['height'] - 20),
 			'click', str(1)
 		]
-		retcode = subprocess.check_call(xdotool_command)
+		subprocess.check_call(xdotool_command, env = virtual_display, stdout = sys.stdout)
 		
-	def start_capture(self, rate, output_file_name):
-		time.sleep(5)
+	def start_capture(self, output_file_name):
+		time.sleep(10)
 		ffmpeg_command = [
 			'ffmpeg',
 			'-f', 'x11grab',
-			'-video_size', '{}x{}'.format(self.width, self.height),
-			#'-i', '127.0.0.1:44',
-			'-i', ':44',
+			'-video_size', '{}x{}'.format(self.swf['width'], self.swf['height']),
+			'-i', '127.0.0.1:44',
 			'-codec:v', 'libx264',
-			'-r', str(rate),
+			'-r', str(self.swf['rate']),
 			output_file_name
 		]
 		self.ffmpeg = subprocess.Popen(ffmpeg_command, stdout=sys.stdout)
 	
-	#def wait_until_finished(self):
-		#time.sleep(10)
+	def wait_until_finished(self):
+		time.sleep(10)
 		#self.gnash.wait()
-		#ffmpeg_on_quite = self.ffmpeg.communicate(bytes(ord('q')))
+		ffmpeg_on_quite = self.ffmpeg.communicate(input = 'q')
 	#	print('--------------------------------------------')
 	#	print(ffmpeg_on_quite.stdout.read())
 	#	print(ffmpeg_on_quite.stderr.read())
@@ -79,10 +82,10 @@ class Screen:
 	
 input_file_name = '../video/tricky.swf'
 swf = explore_swf_file(input_file_name)
-screen = Screen(swf['width'], swf['height'], input_file_name)
-#screen.start_capture(swf['rate'], '/tmp/super.mp4')
+screen = Screen(swf, input_file_name)
+screen.start_capture('/tmp/super.mp4')
 screen.start_playing()
-#screen.wait_until_finished()
+screen.wait_until_finished()
 
 input()
 
