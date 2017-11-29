e7d598c25c4a521598b2d689ab149341e6249c35
flaz14 <flazfourteen@gmail.com>
2017-11-29 18:46:50 +0300

    java-8-whats-new: start new part

---
+++ java-8-whats-new/code/java-8-whats-new/src/main/java/lambda/thiz/LambdaVsAnonymousClass.java
@@ -0,0 +1,67 @@
+package lambda.thiz;
+
+import java.util.stream.Stream;
+
+public class LambdaVsAnonymousClass {
+    public static void main(String args[]) {
+        final SomeClass someObject = new SomeClass();
+        someObject.doWorkWithLambda();
+        someObject.doWorkWithAnonymousInnerClass();
+        someObject.doWorkWithStandaloneClass();
+    }
+
+
+}
+
+@FunctionalInterface
+interface SomeFunction {
+    void func();
+}
+
+class SomeClass {
+    public void doWorkWithLambda() {
+        System.out.println("------------------------------------------------------");
+        final SomeFunction function = () -> {
+            System.out.println("doWorkWithLambda(): " + this.getClass().getName());
+            StacktracePrinter.print();
+        };
+        function.func();
+        System.out.println("------------------------------------------------------");
+    }
+
+    public void doWorkWithAnonymousInnerClass() {
+        System.out.println("------------------------------------------------------");
+        final SomeFunction function = new SomeFunction() {
+            @Override
+            public void func() {
+                System.out.println("doWorkWithAnonymousInnerClass(): " + this.getClass().getName());
+                StacktracePrinter.print();
+            }
+        };
+        function.func();
+        System.out.println("------------------------------------------------------");
+    }
+
+    public void doWorkWithStandaloneClass() {
+        System.out.println("------------------------------------------------------");
+        final SomeFunction function = new StandaloneClass();
+        function.func();
+        System.out.println("------------------------------------------------------");
+    }
+}
+
+class StandaloneClass implements SomeFunction {
+    @Override
+    public void func() {
+        System.out.println("StandaloneClass: " + this.getClass().getName());
+        StacktracePrinter.print();
+    }
+}
+
+class StacktracePrinter {
+    public static void print() {
+        final StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+        Stream.of(stacktrace).
+                forEach(System.out::println);
+    }
+}
