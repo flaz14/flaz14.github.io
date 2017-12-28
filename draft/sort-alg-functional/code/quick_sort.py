8e6fd27b1332ea5ecb55828d4f3fdcd80f94a967
flaz14 <flazfourteen@gmail.com>
2017-12-28 19:49:32 +0300

    sort-alg-functional: start with functional approach

--- draft/sort-alg-functional/code/quick_sort.py
+++ draft/sort-alg-functional/code/quick_sort.py
@@ -1,6 +1,40 @@
 # -*- coding: utf-8 -*-
 
 
+def sort_procedural(input_tuple):
+	def sort_part(array, left_boundary, right_boundary):
+		def get_pivot(array, left_boundary, right_boundary):
+			return array[left_boundary + (right_boundary - left_boundary) // 2]
+		
+		def swap(array, left, right):
+			array[left], array[right] = array[right], array[left]
+		
+		pivot = get_pivot(array, left_boundary, right_boundary)
+		left = left_boundary
+		right = right_boundary
+		while left < right:
+			while array[left] < pivot:
+				left += 1
+			while array[right] > pivot:
+				right -= 1
+			if left <= right:
+				swap(array, left, right)
+				left += 1
+				right -= 1
+		if right > left_boundary:
+			sort_part(array, left_boundary, right)
+		if left < right_boundary:
+			sort_part(array, left, right_boundary)
+	
+	assert input_tuple
+	array = list(input_tuple)
+	sort_part(array, 0, len(array) - 1);
+	return tuple(array)
+
+
+########################################################################################################################
+
+
 def sort(input_tuple):
 	def sort_part(array, left_boundary, right_boundary):
 		def get_pivot(array, left_boundary, right_boundary):
