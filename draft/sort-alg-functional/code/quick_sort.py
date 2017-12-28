109145b666e82cf0702d3d31e8dbfba505009f52
flaz14 <flazfourteen@gmail.com>
2017-12-28 20:15:59 +0300

    sort-alg-functional: functional approach - just convert tabs to spaces

--- draft/sort-alg-functional/code/quick_sort.py
+++ draft/sort-alg-functional/code/quick_sort.py
@@ -36,29 +36,28 @@ def sort_procedural(input_tuple):
 
 
 def sort(input_tuple):
-        def sort_part(array, left_boundary, right_boundary):
-                pivot = array[left_boundary + (right_boundary - left_boundary) // 2]
-                left = left_boundary
-                right = right_boundary
-                while left < right:
-                        while array[left] < pivot:
-                                left += 1
-                        while array[right] > pivot:
-                                right -= 1
-                        if left <= right:
-                                array[left], array[right] = array[right], array[left]
-                                left += 1
-                                right -= 1
-                if right > left_boundary:
-                        sort_part(array, left_boundary, right)
-                if left < right_boundary:
-                        sort_part(array, left, right_boundary)
-                
-        assert input_tuple
-        array = list(input_tuple)
-        sort_part(array, 0, len(array) - 1);
-        return tuple(array)
+	def sort_part(array, left_boundary, right_boundary):
+		pivot = array[left_boundary + (right_boundary - left_boundary) // 2]
+		left = left_boundary
+		right = right_boundary
+		while left < right:
+			while array[left] < pivot:
+				left += 1
+			while array[right] > pivot:
+				right -= 1
+			if left <= right:
+				array[left], array[right] = array[right], array[left]
+				left += 1
+				right -= 1
+		if right > left_boundary:
+			sort_part(array, left_boundary, right)
+		if left < right_boundary:
+			sort_part(array, left, right_boundary)
 
+	assert input_tuple
+	array = list(input_tuple)
+	sort_part(array, 0, len(array) - 1);
+	return tuple(array)
 
 
 if __name__ == '__main__':
