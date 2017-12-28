c69c9947e0ed56613054cd999ed65fd5dfc66b8f
flaz14 <flazfourteen@gmail.com>
2017-12-28 19:29:05 +0300

    add more tests for quick_sort

--- draft/sort-alg-functional/code/test/test_quick_sort.py
+++ draft/sort-alg-functional/code/test/test_quick_sort.py
@@ -17,11 +17,16 @@ def test_none():
 		quick_sort.sort(None)
 
 
-def test_one_item_array():
+def test_one_item_in_array():
 	sorted_array = quick_sort.sort((2,))
 	assert (2,) == sorted_array
 
 
-def test_two_items_array():
+def test_two_items_in_array():
 	sorted_array = quick_sort.sort((5, 3))
-	assert (3, 5) == sorted_array	
+	assert (3, 5) == sorted_array
+
+
+def test_many_items_in_array():
+	sorted_array = quick_sort.sort((88, 10, 3, 8, 15, 1, 5, 4, 2, 18, 55))
+	assert (1, 2, 3, 4, 5, 8, 10, 15, 18, 55, 88) == sorted_array
