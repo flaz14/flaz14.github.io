2f714c5f3bac12c5344d79b8ccfde254d0e60ee0
flaz14 <flazfourteen@gmail.com>
2018-01-13 14:43:15 +0300

    java-self-organized-tracer: add existing source code

---
+++ draft/java-self-organized-tracer/code/sam-tracer/loader/src/main/java/io/github/flaz14/JavaAgent.java
@@ -0,0 +1,18 @@
+package io.github.flaz14;
+
+/**
+ *
+ */
+public class JavaAgent {
+    public static String extractedJar() {
+        return null;
+    }
+
+    /**
+     * Path to the agent's JAR is not hardcoded but will be read from the properties file
+     * (the path to alienated JAR is provided by Maven via resource filtering mechanism).
+     * Actually the path is hardcoded in the corresponding POM. But at least it's hardcoded
+     * one time and in one place.
+     */
+    private static final String JAVA_AGENT_META_FILE = "java-agent-meta.properties";
+}
---
+++ draft/java-self-organized-tracer/code/sam-tracer/loader/src/test/java/io/github/flaz14/test/unit/JvmTest.java
@@ -0,0 +1,23 @@
+package io.github.flaz14.test.unit;
+
+import io.github.flaz14.Jvm;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Enclosed.class)
+public class JvmTest {
+    public static class Pid {
+        @Test
+        public void oracleJvm() {
+            assertThat(Jvm.pid(), is(not(0)));
+        }
+
+
+    }
+}
+
