b137d5e2faec2080a62385e3e8139ba630e5f5a9
flaz14 <flazfourteen@gmail.com>
2018-01-13 15:40:37 +0300

    java-self-organized-tracer: use additional properties

--- draft/java-self-organized-tracer/code/sam-tracer/loader/src/main/java/io/github/flaz14/JavaAgent.java
+++ draft/java-self-organized-tracer/code/sam-tracer/loader/src/main/java/io/github/flaz14/JavaAgent.java
@@ -1,12 +1,29 @@
 package io.github.flaz14;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  *
  */
 public class JavaAgent {
-    public static String extractedJar() {
-//        ClassLoader.
-        return null;
+    public static String pathWithinJar() {
+        try (final InputStream propertiesFile = ClassLoader.getSystemResourceAsStream(PROPERTIES_FILE)) {
+            return pathToJar(propertiesFile);
+        } catch (IOException onLoadingProperties) {
+            throw new IllegalStateException(errorMessage(), onLoadingProperties);
+        }
+    }
+
+    private static String pathToJar(final InputStream propertiesFile) throws IOException {
+        final Properties properties = new Properties();
+        properties.load(propertiesFile);
+        return properties.getProperty("jar");
+    }
+
+    private static String errorMessage() {
+        return String.format("Cannot read Java Agent properties file [%s] from within JAR.", PROPERTIES_FILE);
     }
 
     /**
@@ -15,5 +32,5 @@ public class JavaAgent {
      * Actually the path is hardcoded in the corresponding POM. But at least it's hardcoded
      * one time and in one place.
      */
-    private static final String JAVA_AGENT_META_FILE = "java-agent.properties";
+    private static final String PROPERTIES_FILE = "java-agent.properties";
 }
---
+++ draft/java-self-organized-tracer/code/sam-tracer/loader/src/test/java/io/github/flaz14/test/unit/BootstrapperTest.java
@@ -0,0 +1,15 @@
+package io.github.flaz14.test.unit;
+
+import io.github.flaz14.Bootstrapper;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class BootstrapperTest {
+    @Test
+    public void test() throws Exception {
+        new Bootstrapper();
+
+    }
+}
