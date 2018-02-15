1b64aa1a1f8ec3e56e90ea14668965e364a393da
flaz14 <flazfourteen@gmail.com>
2018-02-15 17:26:28 +0300

    how-to-set-up-offline-software-repository: add warning about virtual packages

--- draft/how-to-set-up-offline-software-repository/code/download.py
+++ draft/how-to-set-up-offline-software-repository/code/download.py
@@ -2,7 +2,38 @@
 
 # -*- coding: utf-8 -*-
 
-ROOT = 'http://archive.ubuntu.com/ubuntu/dists/'
 
-class UbuntuSpider(scrapy.Spider):
-	name 
+import subprocess
+import sys
+
+
+def default_encoding():
+	return 'utf-8'
+
+
+def get_package_names():
+	apt_cache_search_command = [
+		'apt-cache',
+		'pkgnames'
+	]
+	raw_output = subprocess.check_output(
+		apt_cache_search_command,
+		stdin = subprocess.DEVNULL,
+		stderr = sys.stdout
+	)
+	decoded_output = str(
+		raw_output, 
+		default_encoding()
+	)
+	line_by_line_output = decoded_output.splitlines()
+	print(line_by_line_output)
+
+
+
+def main():
+	print("Hello from Downloader!")
+	get_package_names()
+
+
+if __name__ == '__main__':
+	main()	
