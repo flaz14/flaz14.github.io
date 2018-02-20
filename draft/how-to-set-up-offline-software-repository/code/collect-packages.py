3fb40b635d3beb66dcc5d34479d9932c1a0707ab
flaz14 <flazfourteen@gmail.com>
2018-02-20 11:12:26 +0300

    how-to-set-up-offline-software-repository: just save progress

--- draft/how-to-set-up-offline-software-repository/code/collect-packages.py
+++ draft/how-to-set-up-offline-software-repository/code/collect-packages.py
@@ -18,21 +18,38 @@ def get_package_names():
 	]
 	raw_output = subprocess.check_output(
 		apt_cache_search_command,
-		stdin = subprocess.DEVNULL,
-		stderr = sys.stdout
+		stdin = subprocess.DEVNULL
 	)
 	decoded_output = str(
 		raw_output, 
 		default_encoding()
 	)
 	line_by_line_output = decoded_output.splitlines()
-	print(line_by_line_output)
+	return line_by_line_output
 
+
+def get_package_versions(package_names):
+	for name in package_names:
+		apt_cache_showpkg_command = [
+			'apt-cache',
+			'showpkg',
+			name
+		]
+		raw_output = subprocess.check_output(
+			apt_cache_showpkg_command,
+			stdin = subprocess.DEVNULL
+		)
+		decoded_output = str(
+			raw_output, 
+			default_encoding()
+		)
+		print(decoded_output)
 
 
 def main():
-	print("Hello from Downloader!")
-	get_package_names()
+	get_package_versions(
+		get_package_names()
+	)
 
 
 if __name__ == '__main__':
