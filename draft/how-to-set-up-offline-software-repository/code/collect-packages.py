ca3574888b0f00b93c9f466a49b2e328d1e9cc4d
flaz14 <flazfourteen@gmail.com>
2018-02-21 19:45:26 +0300

    how-to-set-up-offline-software-repository: implement first dirty parsing

--- draft/how-to-set-up-offline-software-repository/code/collect-packages.py
+++ draft/how-to-set-up-offline-software-repository/code/collect-packages.py
@@ -28,8 +28,36 @@ def get_package_names():
 	return line_by_line_output
 
 
-def split_by_empty_lines(all_lines):
-	pass
+def extract_text_till_versions_ending(full_output):
+	"""
+		Typical output of `apt-cache showpkg' command includes a lot of text.
+		
+		At the very beginning there is the name of the package. Package's versions are listed below. Then two empty 
+		lines reside. And after the empty lines other information is listed.
+		
+		Those two empty lines is a good marker of ending of the 'versions' sections. Actually, we need to split the 
+		initial output by three new line characters.
+	"""
+	versions_text = full_output.split('\n\n\n')
+	return versions_text[0]
+
+
+
+def extract_versions_strings(versions_text):
+	"""
+		Each versions is separated from the following version by empty line. So in order to extract lonely strings that 
+		contain versions we need split versions text by two new line characters.
+		TODO explain more
+	"""
+	versions_strings = versions_text.split('\n\n')
+	lonely_versions_strings = []
+	first_version_string = versions_strings[0].splitlines()[2]
+	lonely_versions_strings.append(first_version_string)
+	remaining_versions_text = versions_strings[1:]
+	for version_section in remaining_versions_text:
+		version_string = version_section.splitlines()[0]
+		lonely_versions_strings.append(version_string)
+	return lonely_versions_strings
 
 
 def get_package_versions(package_names):
@@ -49,20 +77,10 @@ def get_package_versions(package_names):
 		)
 		print(decoded_output)
 		print('*************************************')
-		versions_text_all = decoded_output.split('\n\n\n')[0]
-		print(versions_text_all)
-		print('*************************************')
-		versions_text = versions_text_all.split('\n\n')
-		print(versions_text)
-		print('*************************************')
-		first_version_text = versions_text[0].splitlines()[2]
-		second_version_text = versions_text[1].splitlines()[0]
-		print(first_version_text)
-		print(second_version_text)
-		print('*************************************')
-		#line_by_line_output = decoded_output.splitlines()
-		#print(line_by_line_output)
-		#parse_package_versions(line_by_line_output)
+		versions_strings = extract_versions_strings(
+			extract_text_till_versions_ending(decoded_output)
+		)
+		print(versions_strings)
 		
 		
 
