e79d59500738c5662423890238ce36bdc13c9dc2
flaz14 <flazfourteen@gmail.com>
2018-02-24 15:27:11 +0300

    how-to-set-up-offline-software-repository: write a lot of comments

--- draft/how-to-set-up-offline-software-repository/code/main/collect-packages.py
+++ draft/how-to-set-up-offline-software-repository/code/main/collect-packages.py
@@ -12,11 +12,11 @@ import json
 This script collects names and corresponding versions of all the packages available on the system. This is done via
 stupid interaction with `apt-cache' command line utility.
 
-Results (in form of JSON) will be printed to STDOUT. Progress indicators will be printed to STDERR. Perhaps, the best 
-way to save the results is to redirect STDOUT to a file in the shell.
+Results (in the form of JSON) will be printed to STDOUT. Progress indicators will be printed to STDERR. Perhaps, the 
+best way to save the results is to redirect STDOUT to a file in the shell.
 
-Also, it's strongly recommended to update apt's cache before running the script (I'm too lazy to handle this situation
-gracefully in my code).
+Also, it's strongly recommended to update apt's cache before running the script (sorry, I'm too lazy to handle this 
+situation gracefully in my code).
 """
 
 
@@ -42,8 +42,8 @@ def logged(obj, message = None):
 	"""
 	Prints object and message specified and then simply returns the object.
 	
-	This function is indended for debugging. At the same time helps avoid dummy local variables and keep functional 
-	programming style going.
+	This function is indended for debugging. At the same time it helps to avoid dummy local variables and keep 
+	functional programming style going.
 	"""	
 	if Settings.logging_enabled():
 		message =	'{}: [{}]'.format(message, obj) if message else \
@@ -68,6 +68,7 @@ class ProgressIndicator:
 			indicator = '{} {:4}%'.format(message, percents)
 			eprint(indicator)
 
+
 class AptCacheCommand:
 	@staticmethod
 	def showpkg(package_name):
@@ -91,7 +92,8 @@ def all_packages_versions(package_names):
 			def versions_strings(versions_text):
 				def versions(versions_strings):
 					"""
-					Version is the very first word in the string, e.g. the sequence of letters and digits before first space.
+					Version is the first word in the string. It's separated from the following words by space. So it's 
+					pretty easy to extract version of a package without using regular expression.
 					"""
 					result = []
 					for string in versions_strings:
@@ -102,13 +104,16 @@ def all_packages_versions(package_names):
 				
 				def top_version_string(versions_sections):
 					"""
-					At the very top section there are 
-					
-					`Package: account-plugin-yahoojp" string followed by `Versions: string. Real version is located in the third string.
+					In order to extract top version string we need to sort out package name (it goes at the very top of 
+					`apt-cache' output) and constant `Versions:' string (it's the second line of the output). Package
+					version is located in the third string of the first section.
 					"""
 					return versions_sections[0].splitlines()[2]
 				
 				def remaining_versions_strings(versions_sections):
+					"""
+					In case of other sections (except the first one) version is located at the first string.
+					"""
 					remaining_sections = versions_sections[1:]
 					result = []
 					for section in remaining_sections:
@@ -116,35 +121,42 @@ def all_packages_versions(package_names):
 							section.splitlines()[0]
 						)
 					return result
-				
-	
+			
 				"""
-				Each versions is separated from the following version by empty line. So in order to extract lonely strings that 
-				contain versions we need split versions text by two new line characters.
-		
-				TODO explain more
+				Usually, there are several versions of the same package in repository. Each version is printed by 
+				`apt-cache' at their own section (it includes additional info like hash sums). Sections are separated 
+				from each other by empty line. 
+				
+				We need to split the text by two new line characters (one for empty line and one for the ending of the 
+				last section).
 				"""
 				versions_sections = versions_text.split('\n\n')
 				return	[top_version_string(versions_sections)] +\
 						remaining_versions_strings(versions_sections)
-			
+
 			"""
-			Typical output of `apt-cache showpkg' command includes a lot of text.
+			Typical output of `apt-cache showpkg' command includes a lot of text. Name of the package is printed at the
+			very beginning. After them package versions are listed. And then other information (direct dependencies,
+			reverse dependencies, etc) goes.
 			
-			At the very beginning there is the name of the package. Package's versions are listed below. Then two empty 
-			lines reside. And after the empty lines other information is listed.
-			
-			Those two empty lines is a good marker of ending of the 'versions' sections. Actually, we need to split the 
-			initial output by three new line characters.
+			Text that contains the versions is separated from the rest of the output by two empty lines. So in order to 
+			extract versions-related stuff we need to split the whole output of `apt-cache showpkg' command by three new
+			line characters (two empty lines and the ending of the last version listed).
 			"""
 			return str(
 				subprocess.check_output(
-					apt_cache_showpkg_command(package_name),
+					AptCacheCommand.showpkg(package_name),
 					stdin = subprocess.DEVNULL
 				),
 				default_encoding()
 			).split('\n\n\n')[0]
 		
+		"""
+		Parsing of the output of `apt-cache showpkg' command needs a lot of work (actually, it can be done without 
+		regular expressions!). Please look at sub-functions for details.
+		
+		The sample output is provided as a file in `sample' directory.
+		"""
 		return versions(
 			versions_strings(
 				versions_text(package_name)
@@ -171,7 +183,7 @@ def all_packages_versions(package_names):
 def all_packages_names():
 	return str(
 		subprocess.check_output(
-			apt_cache_search_command(),
+			AptCacheCommand.search(),
 			stdin = subprocess.DEVNULL
 		),
 		default_encoding()
