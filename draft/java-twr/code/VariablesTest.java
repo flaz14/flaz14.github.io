db5efeaddc5caeea672548f84677bc6c55ce82fd
flaz14 <flazfourteen@gmail.com>
2018-02-24 18:11:17 +0300

    java-twr: add draft of the new article

---
+++ draft/java-twr/code/VariablesTest.java
@@ -0,0 +1,16 @@
+public class VariablesTest {
+	public static void main(String ignored[]) {
+		try( MyAutoCloseable resource = new MyAutoCloseable() ) { }
+		
+		//try( AutoCloseable resource = new MyAutoCloseable() ) {}
+	
+		//try( new MyAutoCloseable() ) { }
+	}
+}
+
+class MyAutoCloseable implements AutoCloseable {
+	@Override
+	public void close() {
+		System.out.println(">>> close()"); 
+	}
+}
