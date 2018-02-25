8ca791205d4cd8743df7266e22896619561eddf0
flaz14 <flazfourteen@gmail.com>
2018-02-25 18:30:37 +0300

    java-twr: start the text

--- draft/java-twr/code/VariablesTest.java
+++ draft/java-twr/code/VariablesTest.java
@@ -2,7 +2,7 @@ public class VariablesTest {
 	public static void main(String ignored[]) {
 		try( MyAutoCloseable resource = new MyAutoCloseable() ) { }
 		
-		//try( AutoCloseable resource = new MyAutoCloseable() ) {}
+		//try( AutoCloseable resource = new MyAutoCloseable() ) { }
 	
 		//try( new MyAutoCloseable() ) { }
 	}
