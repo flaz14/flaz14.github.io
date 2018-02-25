4c4b6ff68e955ce05989548f0153b8a943563940
flaz14 <flazfourteen@gmail.com>
2018-02-25 19:24:18 +0300

    java-twr: write paragraph about lambda expressions

--- draft/java-twr/code/LambdaTest.java
+++ draft/java-twr/code/LambdaTest.java
@@ -1,18 +1,19 @@
 public class LambdaTest {
 	public static void main(String ignored[]) {
+		System.out.println("--- Lambda #1 ---");
 		try (
 			MyAutoCloseable resource = () -> {
 				System.out.println(">>> Lambda #1 close()");
 			}
 		) {}
-		
-		System.out.println("---------------------");
-		
+		System.out.println("-----------------");
+
+		System.out.println("--- Lambda #2 ---");
 		try (
 			MyAutoCloseable resource = () -> {
 				System.out.println(">>> Lambda #2 close()");
 				System.out.println("Stacktrace:");
-				throw new IllegalStateException("Just for illustration.");
+				throw new IllegalStateException("Just for illustration");
 			}
 		) {}
 	}
