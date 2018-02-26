f62c321905d32ee203baca6f5bff526be279661d
flaz14 <flazfourteen@gmail.com>
2018-02-26 12:10:12 +0300

    java-twr: complete the paragraph about suppressed exceptions

--- draft/java-twr/code/SuppressedExceptionTest.java
+++ draft/java-twr/code/SuppressedExceptionTest.java
@@ -1,6 +1,7 @@
 public class SuppressedExceptionTest {
 	public static void main(String ignored[]) {
 		try(MyResource resource = new MyResource()) {
+			System.out.println(">>> try-block");
 			throw new IllegalStateException("Exception from try-block");
 		} finally {
 			System.out.println(">>> finally-block");
