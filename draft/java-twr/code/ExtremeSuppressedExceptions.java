73a601d26c0671a07c6bd4d4d97add3834a9d4f4
flaz14 <flazfourteen@gmail.com>
2018-02-26 13:51:13 +0300

    java-twr: just save progress

--- draft/java-twr/code/ExtremeSuppressedExceptions.java
+++ draft/java-twr/code/ExtremeSuppressedExceptions.java
@@ -1,4 +1,23 @@
 public class ExtremeSuppressedExceptions {
-
+	public static void main(String ignored[]) {
+		try ( MyResource resource = new MyResource() ) {
+			System.out.println(">>> main()");
+			throw new IllegalStateException("Exception from main()");
+		}
+	}
 }
 
+class MyResource implements AutoCloseable {
+	@Override
+	public void close() {
+		System.out.println(">>> MyResource.close()");
+		try {
+			System.out.println(">>> try-block from MyResource");
+			throw new IllegalStateException("Exception from try-block from MyResource");
+		}
+		finally {
+			System.out.println(">>> finally-block from MyResource");
+			throw new IllegalStateException("Exception from finally-block from MyResource");
+		}
+	}
+}
