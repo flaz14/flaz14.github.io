cbd6175d375403fb917004b89d58b95cad6b8c82
flaz14 <flazfourteen@gmail.com>
2018-05-07 12:35:51 +0300

    tetris-python-dos: commit the last findings and abandone the article (may be not forever...)

--- downgrade/tetris-python-dos/handyfiles/autoexec.bat
+++ downgrade/tetris-python-dos/handyfiles/autoexec.bat
@@ -1,5 +1,6 @@
 C:\doslfn\doslfn
+SET PYTHONCASEOK=1
 SET PYTHONHOME=C:/pythond
-rem SET PYTHONPATH=C:/pythond/lib/python2.4
-SET PYTHONPATH=C:\pythond\lib\python2.4
 PATH c:\pythond\bin\python\;%PATH%
+SET PATH=C:\DJGPP\BIN;%PATH%
+SET DJGPP=C:\DJGPP\DJGPP.ENV
