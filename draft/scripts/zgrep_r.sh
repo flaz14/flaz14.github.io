24da3ed5278b1ae44c06726e25ada17b41ade6e5
flaz14 <flazfourteen@gmail.com>
2018-05-31 13:51:46 +0300

    dgmag.in: implement grep in zip files

--- draft/scripts/zgrep_r.sh
+++ draft/scripts/zgrep_r.sh
@@ -6,13 +6,13 @@ function punctual_zgrep {
 	local temp_dir="$( mktemp --directory --tmpdir=/dev/shm/ )"
 	7z x "$file" -o"$temp_dir" >/dev/null 2>&1
 	local temp_file="$( find "$temp_dir" -type f -print0 )"
-	#echo "$temp_file"
-	echo -n "$file: " 
-	grep --no-filename "$text" "$temp_file"
+	echo "$file: "
+	grep --no-filename --color=always "$text" "$temp_file"
 }
 
 text="$1"
 shift
 find "$@" -type f -print0 | while IFS= read -r -d '' file; do 
 	punctual_zgrep "$text" "$file"; 
+	echo "--"
 done
