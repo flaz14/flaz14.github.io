104ba03a76f54bbbb5bf9efacef10a33b7e0e39a
flaz14 <flazfourteen@gmail.com>
2018-06-22 19:48:55 +0300

    java-crackme: start new article

---
+++ draft/java-crackme/code/App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Objects;
+
+public class App {
+	private static final String VALID_SERIAL_NUMBER = "qwerty";	
+	
+	public static void main(String ignoredArgs[]) {
+		System.out.println(	"****************************************\n" +
+							"* SuperMega Application by flaz14 Inc. *\n" + 
+							"****************************************\n" +
+							"\n" + 
+							"Hello, User!" + 
+							"\n" + 
+							"Please enter your Serial Number below\n" +
+							"and hit ENTER: " + 
+							"\n");
+		String rawSerialNumber = null;
+		try (Scanner keyboard = new Scanner(System.in, "UTF-8")) {
+			if (keyboard.hasNextLine())
+				rawSerialNumber = keyboard.nextLine();
+		}
+		if (rawSerialNumber == null) 
+			throw new IllegalStateException("Internal error: serialNumber" +
+											"should not be null.");
+		String serialNumber = rawSerialNumber.trim();
+		if ( Objects.equals(serialNumber, VALID_SERIAL_NUMBER) )
+			System.out.println(	"\nCongratulations! You've successfully\n" +
+								"activated your copy of the application.");
+		else 
+			System.out.println( "\nSorry, the serial you've entered is invalid.");
+	}
+}
