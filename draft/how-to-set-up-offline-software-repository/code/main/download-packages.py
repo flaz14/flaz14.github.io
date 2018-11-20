d98e0df73ff3087a2eac30809834373e556dee29
flaz14 <flazfourteen@gmail.com>
2018-11-20 16:47:25 +0300

    how-to-set-up-offline-software-repository: just save progress

--- draft/how-to-set-up-offline-software-repository/code/main/download-packages.py
+++ draft/how-to-set-up-offline-software-repository/code/main/download-packages.py
@@ -9,6 +9,38 @@ import sys
 import subprocess
 
 
+class Settings:
+	@staticmethod
+	def logging_enabled():
+		return True
+
+	@staticmethod
+	def default_encoding():
+		return 'utf-8'
+
+
+def eprint(*args, **kwargs):
+	"""
+	This function is copied-and-pasted from 
+	[How to print to stderr in Python?](https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python).
+	"""
+	print(*args, file=sys.stderr, **kwargs)
+
+
+def logged(obj, message = None):
+	"""
+	Prints object and message specified and then simply returns the object.
+	
+	This function is indended for debugging. At the same time it helps to avoid dummy local variables and keep 
+	functional programming style going.
+	"""	
+	if Settings.logging_enabled():
+		message =	'{}: [{}]'.format(message, obj) if message else \
+					'{}'.format(obj)
+		eprint(message)
+	return obj
+
+
 class AptGetCommand:
 	@staticmethod
 	def source(package_name):
@@ -35,27 +67,43 @@ def lines_from_file(file_descriptor):
        )
 
 
+def ignorable_apt_get_error():
+	return 'E: Unable to find a source package for'
+
+
+def download_source(package_name):
+	process = subprocess.Popen(
+		AptGetCommand.source(
+			logged(package_name, 'Downloading source for package')
+		), 
+		stdin = subprocess.DEVNULL,
+		stdout = subprocess.PIPE,
+		stderr = subprocess.PIPE
+	)
+	out, err = process.communicate()
+	error_text = str(err, 'utf-8')
+	if ignorable_apt_get_error() not in error_text:
+		exception_message = 'Failed to download source for package [{}] due to [{}].'.format(
+			package_name,
+			error_text
+		)
+		raise Exception(exception_message)	
+
+	
+def download_deb(package_name):
+	subprocess.check_output(
+		AptGetCommand.download(
+			logged(package_name, 'Downloading .deb-file for package')
+		),
+		stdin = subprocess.DEVNULL
+	)
+
+
 def main():
-	package_names = lines_from_file(sys.stdin)
-	for package_name in package_names:
-		aptGetSourceProcess = subprocess.Popen(
-			AptGetCommand.source(package_name), 
-			stderr=subprocess.PIPE,
-			stdout=subprocess.PIPE
-		)
-		out, err = aptGetSourceProcess.communicate() 
-		error = str(err, 'utf-8')
-		if 'E: Unable to find a source package for' not in error:
-			print('unexpected error:', error)
-			raise Exception('shit')	
-		download_output = str(
-			subprocess.check_output(
-				AptGetCommand.download(package_name),
-				stdin = subprocess.DEVNULL
-			),
-			'utf-8'
-		)
-		print('Download output:', download_output)
+	packages = lines_from_file(sys.stdin)
+	for package in packages:
+		download_source(package)
+		download_deb(package)
 
 
 if __name__ == '__main__':
